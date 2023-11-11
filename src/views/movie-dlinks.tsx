079a14f8a33577fceb948469bd9dc378127d5f0a
Oluwatobi <krisella74@gmail.com>
2023-11-11 23:34:24 +0100

    show loading state

--- src/views/movie-dlinks.tsx
+++ src/views/movie-dlinks.tsx
@@ -38,7 +38,7 @@ export default function MovieDownloadLinks() {
           </div>
         </div>
       ) : (
-        <LoadingOverlay />
+        <LoadingOverlay visible={isLoading} />
       )}
 
       <style jsx>{`
